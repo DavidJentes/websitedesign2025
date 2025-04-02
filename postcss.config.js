5305f089288097dd23fd54864586770b3f0bd1d8
daviddgtnt <david@daviddgtnt.xyz>
2025-04-02 15:02:20 -0500

    Upgrade to Tailwind 4

--- postcss.config.js
+++ postcss.config.js
@@ -1,10 +1,5 @@
 module.exports = {
-plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-    require('postcss-google-font'),
-    ...(process.env.JEKYLL_ENV == 'production'
-        ? [require('cssnano')({ preset: 'default' })]
-        : [])
+    plugins: [
+        require('@tailwindcss/postcss'),
     ]
 }
